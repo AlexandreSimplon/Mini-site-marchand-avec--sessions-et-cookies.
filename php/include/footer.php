60f85ab42bcbc8d4092b9ecef59876834cc21d63
alexandre <alexandre.douchet.pro@gmail.com>
2019-05-14 10:56:08 +0200

    header & footer OK

--- php/include/footer.php
+++ php/include/footer.php
@@ -4,7 +4,6 @@
     </div>
 
 <footer class="page-footer grey darken-4">       
-
         <div class="row linge">
             <div class="col s1 offset-s1"><i class="material-icons">network_locked</i></div>
             <div class="col s2">PAIEMENT SECURISE</div>
@@ -12,6 +11,45 @@
             <div class="col s2">NOUS CONTACTER</div>
             <div class="col s2">NOUS SUIVRE</div>
         </div>
+        <div class="row linge">
+            <div class="col s3 offset-s1">
+                <div class="row linge">
+                    <div class="col s3"><img class="securise" src="/images/visa-5-logo-png-transparent.png" alt="card visa"></div>
+                    <div class="col s3"><img class="securise" src="/images/mastercard-symbol-02.jpg" alt="master card"></div>
+                    <div class="col s3"><img class="securise" src="/images/logo-cb.jpg" alt="c.b"></div>
+                    <div class="col s3"><img class="securise" src="/images/Logo-cic.png" alt="c.i.c"></div>
+                    <div class="col s3"><img class="securise" src="/images/paypal-784403_960_720.png" alt=""></div>
+                    <div class="col s3"><img class="securise" src="/images/66779708-sécurisé-en-ligne-logo-webshop.jpg" alt=""></div>
+                    <div class="col s3"><img class="securise" src="/images/800px-Logo_Oney.svg.png" alt=""></div>
+                    <div class="col s3"><img class="securise" src="/images/logo_fia-net_rouge_bg_0.jpg" alt=""></div>
+                </div>
+            </div>
+            <div class="col s4">
+                <div class="row linge">
+                    <div class="col s2"><img class="securise" src="/images/LOGO-COLIPOSTE.jpg" alt="coli poste"></div>
+                    <div class="col s2"><img class="securise" src="/images/tnt-logo.jpg" alt="t.n.t"></div>
+                </div>
+            </div>
+            <div class="col s2">
+                <div class="row linge">
+                    <div class="col s2">
+                        <p>01.48.75.18.41</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s2">
+
+                <div class="row linge">
+                    <div class="col s3"><img class="securise" src="/images/iconfinder_circle-twitter_317749.png" alt="card visa"></div>
+                    <div class="col s3"><img class="securise" src="/images/iconfinder_google-plus_317741.png" alt="master card"></div>
+                    <div class="col s3"><img class="securise" src="/images/iconfinder_square-facebook_317727.png" alt="card visa"></div>
+                    <div class="col s3"><img class="securise" src="/images/iconfinder_youtube_317714.png" alt="master card"></div>
+                </div>
+
+            </div>
+        </div>
+
+
 
 
         <div class="footer-copyright">
